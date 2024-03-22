fdee3dfb505e08b61e2b31fb3624fcbf6135e9f6
Yuliia <the.smile.too@gmail.com>
2024-03-22 19:39:10 +0200

    update the decimalToBinary function

--- src/js/script.js
+++ src/js/script.js
@@ -8,8 +8,16 @@ const decimalToBinary = (input) => {
   const remainders = [];
 
   while (input > 0) {
-    input = 0;
+    const quotient = Math.floor(input / 2);
+    const remainder = input % 2;
+    inputs.push(input);
+    quotients.push(quotient);
+    remainders.push(remainder);
+    input = quotient;
   }
+  console.log("Inputs: ", inputs);
+  console.log("Quotients: ", quotients);
+  console.log("Remainders: ", remainders);
 };
 
 const checkUserInput = () => {
